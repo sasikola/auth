dac1da27d7b3849a8bffaa2100dc37a43d65dbeb
sasikiran0 <sasikran@bloomitsolution.in>
2024-05-18 22:10:30 +0530

    fix

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const db = require("./db");
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is healthy!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Example app listening on port 3000!");
+});
